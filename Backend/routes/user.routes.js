83930451fb00c78159b527f25cc791e63928dcd3
judilegend <judilegend2.com>
2024-08-07 22:14:34 +0300

    fix/simple-bug

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,7 +6,6 @@ const { authenticate, isAdmin } = require("../middleware/auth.middleware.js");
 // CRUD User routes
 router.post("/", [authenticate, isAdmin], users.create);
 router.get("/", [authenticate, isAdmin], users.findAll);
-router.get("/:id", [authenticate, isAdmin], users.findOne);
 router.put("/:id", [authenticate, isAdmin], users.update);
 router.delete("/:id", [authenticate, isAdmin], users.delete);
 
